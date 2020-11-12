80b5470252275962f7742c8493e0d4ee6004e4be
sebnorth <sebnorth@gmail.com>
2020-11-12 12:45:20 +0100

    Dodaj pliki projektów.

---
+++ Lab7PR1/BinaryHeap.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7PR1
+{
+    class BinaryHeap
+    {
+        int[] arr;
+        int sizeOfHeap;
+
+        public BinaryHeap(int n) {
+
+            this.arr = new int[n + 1];
+            sizeOfHeap = 0;
+        }
+
+
+        public int Parent(int i) {
+
+            return i / 2;
+        }
+
+        public int Left(int i) {
+
+            return 2 * i;
+        }
+
+        public int Right(int i) {
+
+            return 2 * i + 1;
+
+        }
+
+
+        public void Swap(int x, int y) {
+
+            int kopia = arr[x];
+            arr[x] = arr[y];
+            arr[y] = kopia;
+        }
+
+
+        public void moveUp(int i) {
+
+            while (Parent(i) >= 1 && arr[i] > arr[Parent(i)] )
+            {
+
+                Swap(i, Parent(i));
+                i = Parent(i);
+            }
+        }
+
+
+        public void AddToHeap(int item) {
+
+            sizeOfHeap++;
+            arr[sizeOfHeap] = item;
+            moveUp(sizeOfHeap);
+        }
+
+        public void AddToArray(int item) {
+            sizeOfHeap++;
+            arr[sizeOfHeap] = item;
+            // moveUp(sizeOfHeap);
+        }
+
+
+        public void moveDown(int i) { 
+        
+            // TODO
+        }
+
+
+        public void BuildMaxHeap() {
+
+            
+            for (int i = sizeOfHeap / 2; i >= 1; i--)
+            {
+                // TODO
+            }
+        }
+
+        public void WypiszKopiec() {
+
+            for (int i = 1; i <= sizeOfHeap; i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+        }
+
+
+
+    }
+}
